d98a80acd23a39b72be6290836670febc113643b
Genie16-github <4411dlwoqja@naver.com>
2023-03-14 16:57:34 +0900

    문자열 압축

--- src/main/java/study/ll/Section1/Test1/Main.java
+++ src/main/java/study/ll/Section1/Test1/Main.java
@@ -2,14 +2,14 @@ package study.ll.Section1.Test1;
 
 public class Main {
     public String solution(String s){
-        StringBuilder answer= new StringBuilder();
-        s += " ";
+        StringBuilder answer = new StringBuilder();
+        s += " ";  // 끝까지 탐색
         char start = s.charAt(0);
         int cnt = 1;
         for (int i = 1; i < s.length(); i++){
-            if(start == s.charAt(i)) cnt++;
+            if(start == s.charAt(i)) cnt++;  // 다음 문자랑 비교하면서 같으면 cnt 계속 증가
             else{
-                answer.append(start);
+                answer.append(start);        // 다를 경우 cnt 추가하고 start에 다음 문자 삽입
                 if(cnt > 1) answer.append(cnt);
                 cnt = 1;
                 start = s.charAt(i);
